e77c84e5ab8f4fdf2d3b09b4416f24c49f017a87
SANJID-01 <sanjid.cse16@gmail.com>
2025-02-23 23:47:36 +0600

    Add files via upload

---
+++ Week-11/day-6/A_Medium_Number.cpp
@@ -0,0 +1,40 @@
+#define sonic                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define io_files                      \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout); // *required
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pub(x, y) (x).push_back(y)
+#define ppb(x) (x).pop_back()
+#define ff first
+#define ss second
+#define nl '\n'
+#define sz(x) (int)x.size()
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define Minus cout << -1 << '\n'
+#define all(x) (x).begin(), (x).end()
+#define Unique(x) (x).erase(unique(all(x)), (x).end()) // *required sorted
+
+int main()
+{
+    sonic;
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int md = a + b + c - min(a, min(b, c)) - max(a, max(b, c));
+        cout << md << nl;
+    }
+
+    return 0;
+}
