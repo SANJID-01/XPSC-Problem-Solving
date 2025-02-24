011bd9c2deddc7fa7e03a58a0f165ae49f86fb0c
SANJID-01 <sanjid.cse16@gmail.com>
2025-02-24 23:21:42 +0600

    Add files via upload

---
+++ Week-11/day-7/A_Vasya_the_Hipster.cpp
@@ -0,0 +1,34 @@
+#define sonic                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define io_files                      \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout); // *required
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pub(x, y) (x).push_back(y)
+#define ppb(x) (x).pop_back()
+#define ff first
+#define ss second
+#define nl '\n'
+#define sz(x) (int)x.size()
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define Minus cout << -1 << '\n'
+#define all(x) (x).begin(), (x).end()
+#define Unique(x) (x).erase(unique(all(x)), (x).end()) // *required sorted
+
+int main()
+{
+    sonic;
+
+    int a, b;
+    cin >> a >> b;
+
+    int diff = min(a, b);
+    int sm = (max(a, b) - diff) / 2;
+    cout << diff << " " << sm << nl;
+    return 0;
+}
