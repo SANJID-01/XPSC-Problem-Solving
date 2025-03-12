0c34b4c9492e3cc93aca6e8267ff28b7e9e23dab
SANJID-01 <sanjid.cse16@gmail.com>
2025-03-12 23:21:46 +0600

    Add files via upload

---
+++ week-14/day-1/Triangle_Checking.cpp
@@ -0,0 +1,36 @@
+#define sonic                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define io_files                      \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout); // *required
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pub(x, y) (x).push_back(y)
+#define ppb(x) (x).pop_back()
+#define ff first
+#define ss second
+#define nl '\n'
+#define sz(x) (int)x.size()
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define Minus cout << -1 << '\n'
+#define all(x) (x).begin(), (x).end()
+#define Unique(x) (x).erase(unique(all(x)), (x).end()) // *required sorted
+
+int main()
+{
+    sonic;
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if ((a + b) > c && (b + c) > a && (c + a) > b)
+        cout << "Yes" << nl;
+    else
+        cout << "No" << nl;
+
+    return 0;
+}
